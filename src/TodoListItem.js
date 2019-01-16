cbe3da306626b59fd35655c78f55e339f74001cc
Mimi <dartehmimi@gmail.com>
2019-01-16 15:58:24 -0800

    starting list count from 1

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -4,7 +4,7 @@ export default class TodoListItem extends Component {
   render() {
     return (
       <div>
-        <p>{this.props.id}. {this.props.todoListItem}</p>
+        <p>{this.props.id + 1}. {this.props.todoListItem}</p>
       </div>
     )
   }
